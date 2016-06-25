f40fc36b252ee9b2f999c7de9fb054f262d28bd1
巴糖 <380411544@qq.com>
2016-06-25 22:01:45 +0800

    简化代码

--- iOS/UIKit/UITextField+WX.h
+++ iOS/UIKit/UITextField+WX.h
@@ -5,5 +5,8 @@
 @property(nonatomic) UIColor * wx_placeHolderTextColor;
 @property(nonatomic) UIColor * wx_placeHolderTextFont;
 
+- (void)wx_leftViewImageName:(nonnull NSString *)imageName  width:(CGFloat)width;
 
+- (void)wx_leftViewShowText:(nonnull NSString *)text rect:(CGRect)rect;
+- (void)wx_leftViewShowRightText:(nonnull NSString *)text rect:(CGRect)rect;
 @end
--- iOS/UIKit/UITextField+WX.m
+++ iOS/UIKit/UITextField+WX.m
@@ -26,4 +26,39 @@ static NSString * const kTextFieldPlaceholderLabelTextColor = @"_placeholderLabe
     return [self valueForKeyPath:kTextFieldPlaceholderLabelFont];
 }
 
+ - (void)wx_leftViewImageName:(nonnull NSString *)imageName  width:(CGFloat)width
+{
+    UIImageView *iv =[UIImageView wx_imageViewWithImageName:@"Icon_phone"];
+    iv.wx_width = width;
+    iv.contentMode = UIViewContentModeCenter;
+    self.leftView = iv;
+    self.leftViewMode = UITextFieldViewModeAlways;
+}
+
+- (void)wx_leftViewShowText:(nonnull NSString *)text rect:(CGRect)rect
+{
+    UILabel *label = [[UILabel alloc]initWithFrame:rect];
+    label.text = text;
+    self.leftView = label;
+    self.leftViewMode = UITextFieldViewModeAlways;
+    
+}
+
+- (void)wx_leftViewShowRightText:(nonnull NSString *)text rect:(CGRect)rect
+{
+    UILabel *label = [[UILabel alloc]initWithFrame:rect];
+    label.text = text;
+    label.textAlignment = NSTextAlignmentRight;
+    self.leftView = label;
+    self.leftViewMode = UITextFieldViewModeAlways;
+    
+}
+
+
+
+
+
+
+
+
 @end
