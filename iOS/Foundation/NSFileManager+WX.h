c874535ebf19700167a13275ccb0f09fbda32fcc
巴糖 <380411544@qq.com>
2016-06-28 21:14:42 +0800

    优化代码

--- iOS/Foundation/NSFileManager+WX.h
+++ iOS/Foundation/NSFileManager+WX.h
@@ -9,6 +9,56 @@
  *
  *  @return
  */
-- (unsigned long long)fileSizeAtPath:(NSString *)filePath;
+- (unsigned long long)wx_fileSizeAtPath:(NSString *)filePath;
+
+
+
+
+
+//获取沙盒根目录
+
+/**
+ *  获取沙盒根目录
+ *
+ *  @return
+ */
+FOUNDATION_EXPORT NSString *wx_getDirectoryPath(void);
+
+/**
+ *  获取Documents路径
+ *
+ *  @return
+ */
+FOUNDATION_EXPORT NSString *wx_getDocumentsPath(void);
+
+
+/**
+ *  获取Library路径
+ *
+ *  @return
+ */
+FOUNDATION_EXPORT NSString *wx_getLibraryPath(void);
+
+/**
+ *  获取Caches路径
+
+ *
+ *  @return
+ */
+FOUNDATION_EXPORT NSString *wx_getCachesPath(void);
+
+
+/**
+ *  获取tmp路径
+ *
+ *  @return
+ */
+FOUNDATION_EXPORT NSString *wx_getTemporaryDirectoryPath(void);
+
+
+
+
+
+
 
 @end
--- iOS/Foundation/NSFileManager+WX.m
+++ iOS/Foundation/NSFileManager+WX.m
@@ -2,6 +2,9 @@
 
 @implementation NSFileManager (WX)
 
+
+
+
 + (BOOL)wx_createfolder:(NSString *)folder path:(NSString *)path
 {
     NSFileManager *fileManager = [NSFileManager defaultManager];
@@ -16,53 +19,47 @@
     return [fileManager createFileAtPath:iOSPath contents:nil attributes:nil];
 }
 
-//创建文件夹
-- (BOOL)wx_createFolderToDocuments:(NSString *)folder
+//创建文件夹 在Documents
++ (BOOL)wx_createFolderToDocuments:(NSString *)folder
 {
-    NSString *documentsPath =[self wx_getDocumentsPath];
+    NSString *documentsPath = wx_getDocumentsPath();
     return [NSFileManager wx_createfolder:folder path:documentsPath];
 }
 
-//创建文件
-- (BOOL)wx_createFileToDocuments:(NSString *)file
+//创建文件 在Documents
++ (BOOL)wx_createFileToDocuments:(NSString *)file
 {
-    NSString *documentsPath =[self wx_getDocumentsPath];
+    NSString *documentsPath = wx_getDocumentsPath();
     return [NSFileManager wx_createFile:file path:documentsPath];
 }
 
-//写文件
--(void)writeFile
+//写文件 在Documents
++ (BOOL)writeFile:(NSString *)file
 {
-    NSString *documentsPath =[self wx_getDocumentsPath];
-    NSString *iOSPath = [documentsPath stringByAppendingPathComponent:@"iOS.txt"];
+    NSString *documentsPath = wx_getDocumentsPath();
+    NSString *iOSPath = [documentsPath stringByAppendingPathComponent:file];
     NSString *content = @"我要写数据啦";
-    BOOL isSuccess = [content writeToFile:iOSPath atomically:YES encoding:NSUTF8StringEncoding error:nil];
-    if (isSuccess) {
-        NSLog(@"write success");
-    } else {
-        NSLog(@"write fail");
-    }
+    return [content writeToFile:iOSPath atomically:YES encoding:NSUTF8StringEncoding error:nil];
 }
 
-//读取文件内容
--(void)readFileContent
+//读取文件内容 在Documents
+-(void)readFileContent:(NSString *)file
 {
-    NSString *documentsPath =[self wx_getDocumentsPath];
-    NSString *iOSPath = [documentsPath stringByAppendingPathComponent:@"iOS.txt"];
+    NSString *documentsPath = wx_getDocumentsPath();
+    NSString *iOSPath = [documentsPath stringByAppendingPathComponent:file];
     NSString *content = [NSString stringWithContentsOfFile:iOSPath encoding:NSUTF8StringEncoding error:nil];
     NSLog(@"read success: %@",content);
 }
 
 //判断文件是否存在
-- (BOOL)isSxistAtPath:(NSString *)filePath
++ (BOOL)isSxistAtPath:(NSString *)filePath
 {
     NSFileManager *fileManager = [NSFileManager defaultManager];
-    BOOL isExist = [fileManager fileExistsAtPath:filePath];
-    return isExist;
+    return [fileManager fileExistsAtPath:filePath];
 }
 
 //计算文件大小
-- (unsigned long long)fileSizeAtPath:(NSString *)filePath
+- (unsigned long long)wx_fileSizeAtPath:(NSString *)filePath
 {
     NSFileManager *fileManager = [NSFileManager defaultManager];
     BOOL isExist = [fileManager fileExistsAtPath:filePath];
@@ -70,7 +67,6 @@
         unsigned long long fileSize = [[fileManager attributesOfItemAtPath:filePath error:nil] fileSize];
         return fileSize;
     } else {
-        NSLog(@"file is not exist");
         return 0;
     }
 }
@@ -86,89 +82,73 @@
         NSString *fileName = @"";
         while ((fileName = [childFileEnumerator nextObject]) != nil){
             NSString* fileAbsolutePath = [folderPath stringByAppendingPathComponent:fileName];
-            folderSize += [self fileSizeAtPath:fileAbsolutePath];
+            folderSize += [self wx_fileSizeAtPath:fileAbsolutePath];
         }
         return folderSize / (1024.0 * 1024.0);
     } else {
-        NSLog(@"file is not exist");
         return 0;
     }
 }
 
-//删除文件
--(void)deleteFile
+//删除文件 在Documents
++ (BOOL)deleteFile:(NSString *)file
 {
-    NSString *documentsPath =[self wx_getDocumentsPath];
+    NSString *documentsPath = wx_getDocumentsPath();
     NSFileManager *fileManager = [NSFileManager defaultManager];
-    NSString *iOSPath = [documentsPath stringByAppendingPathComponent:@"iOS.txt"];
-    BOOL isSuccess = [fileManager removeItemAtPath:iOSPath error:nil];
-    if (isSuccess) {
-        NSLog(@"delete success");
-    }else{
-        NSLog(@"delete fail");
-    }
+    NSString *iOSPath = [documentsPath stringByAppendingPathComponent:file];
+    return [fileManager removeItemAtPath:iOSPath error:nil];
 }
 
-//移动文件
-- (void)moveFileName
+//移动文件 在Documents
++ (BOOL)moveFileName:(NSString *)sourceFile Tofile:(NSString *)tofile
 {
-    NSString *documentsPath =[self wx_getDocumentsPath];
+    NSString *documentsPath = wx_getDocumentsPath();
     NSFileManager *fileManager = [NSFileManager defaultManager];
-    NSString *filePath = [documentsPath stringByAppendingPathComponent:@"iOS.txt"];
-    NSString *moveToPath = [documentsPath stringByAppendingPathComponent:@"iOS.txt"];
-    BOOL isSuccess = [fileManager moveItemAtPath:filePath toPath:moveToPath error:nil];
-    if (isSuccess) {
-        NSLog(@"rename success");
-    }else{
-        NSLog(@"rename fail");
-    }
+    NSString *filePath = [documentsPath stringByAppendingPathComponent:sourceFile];
+    NSString *moveToPath = [documentsPath stringByAppendingPathComponent:tofile];
+    return [fileManager moveItemAtPath:filePath toPath:moveToPath error:nil];
 }
 
-//重命名
-- (void)renameFileName
+//重命名 在Documents
++ (BOOL)renameFileName:(NSString *)sourceFile renamefile:(NSString *)renamefile
 {
     //通过移动该文件对文件重命名
-    NSString *documentsPath =[self wx_getDocumentsPath];
+    NSString *documentsPath = wx_getDocumentsPath();
     NSFileManager *fileManager = [NSFileManager defaultManager];
-    NSString *filePath = [documentsPath stringByAppendingPathComponent:@"iOS.txt"];
-    NSString *moveToPath = [documentsPath stringByAppendingPathComponent:@"rename.txt"];
-    BOOL isSuccess = [fileManager moveItemAtPath:filePath toPath:moveToPath error:nil];
-    if (isSuccess) {
-        NSLog(@"rename success");
-    }else{
-        NSLog(@"rename fail");
-    }
+    NSString *filePath = [documentsPath stringByAppendingPathComponent:sourceFile];
+    NSString *moveToPath = [documentsPath stringByAppendingPathComponent:renamefile];
+    return [fileManager moveItemAtPath:filePath toPath:moveToPath error:nil];
 }
 
-//获取沙盒根目录
-- (NSString *)wx_getDirectoryPath
+//获取沙盒根目录 
+FOUNDATION_EXPORT NSString *wx_getDirectoryPath(void)
 {
     return NSHomeDirectory();
 }
 
 //获取Documents路径
-- (NSString *)wx_getDocumentsPath
+FOUNDATION_EXPORT NSString *wx_getDocumentsPath(void)
 {
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     return [paths objectAtIndex:0];
 }
 
 //获取Library路径
-- (NSString *)wx_getLibraryPath
+FOUNDATION_EXPORT NSString *wx_getLibraryPath(void)
 {
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
     return [paths objectAtIndex:0];
 }
 
 //获取Caches路径
-- (NSString *)wx_getCachesPath
+FOUNDATION_EXPORT NSString *wx_getCachesPath(void)
 {
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
     return [paths objectAtIndex:0];
 }
 
 //获取tmp路径
-- (NSString *)wx_getTemporaryDirectoryPath
+FOUNDATION_EXPORT NSString *wx_getTemporaryDirectoryPath(void)
 {
     return NSTemporaryDirectory();
 }
