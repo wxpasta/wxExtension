8de26ad595da2f14f515a5bd3cc0d30de6e9e015
巴糖 <380411544@qq.com>
2016-07-10 21:55:58 +0800

    删除

--- Lib/UIImageView+AM.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface UIImageView (AM)
-
-- (void)wx_setImageWithURLString:(NSString *)urlString;
-
-- (void)wx_setImageWithURLString:(NSString *)urlString placeholderImageName:(NSString *)placeholder;
-
-@end
