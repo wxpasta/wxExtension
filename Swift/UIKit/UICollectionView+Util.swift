bfab33559bd901275ef6213915dc4e0f397606a8
巴糖 <380411544@qq.com>
2017-08-07 16:59:18 +0800

    add

--- Swift/UIKit/UICollectionView+Util.swift
+++ Swift/UIKit/UICollectionView+Util.swift
@@ -29,4 +29,13 @@ extension UICollectionView{
         let nib = UINib(nibName: nibName, bundle: Bundle.main)
         self.register(nib, forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: nibName)
     }
+    
+    /// SwifterSwift: Register UICollectionViewCell using class name.
+    ///
+    /// - Parameters:
+    ///   - nib: Nib file used to create the collectionView cell.
+    ///   - name: UICollectionViewCell type.
+    public func register<T: UICollectionViewCell>(nib: UINib?, forCellWithClass name: T.Type) {
+        register(nib, forCellWithReuseIdentifier: String(describing: name))
+    }
 }
