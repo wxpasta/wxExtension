70022f5341413d3609943cc3b4b7a8eed075f22e
许宝吉 <xubaoji@xylink.com>
2019-06-04 14:45:40 +0800

    add Language修改

---
+++ Objective-C/Foundation/NSBundle/NSBundle+XYLanguage.h
@@ -0,0 +1,19 @@
+//
+//  NSBundle+XYLanguage.h
+//  NemoOfficeClient
+//
+//  Created by 许宝吉 on 2019/6/4.
+//  Copyright © 2019 ainemo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSBundle (XYLanguage)
+
++ (void)setLanguage:(NSString *)language;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Objective-C/Foundation/NSBundle/NSBundle+XYLanguage.m
@@ -0,0 +1,40 @@
+//
+//  NSBundle+XYLanguage.m
+//  NemoOfficeClient
+//
+//  Created by 许宝吉 on 2019/6/4.
+//  Copyright © 2019 ainemo. All rights reserved.
+//
+
+#import "NSBundle+XYLanguage.h"
+#import <objc/runtime.h>
+
+static const char _bundle = 0;
+
+@interface BundleEx : NSBundle
+
+@end
+
+@implementation BundleEx
+
+- (NSString *)localizedStringForKey:(NSString *)key
+                              value:(NSString *)value
+                              table:(NSString *)tableName {
+    NSBundle *bundle = objc_getAssociatedObject(self, &_bundle);
+    return bundle ? [bundle localizedStringForKey:key value:value table:tableName] : [super localizedStringForKey:key value:value table:tableName];
+}
+
+@end
+
+@implementation NSBundle (XYLanguage)
+
++ (void)setLanguage:(NSString *)language {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        object_setClass([NSBundle mainBundle], [BundleEx class]);
+    });
+    
+    objc_setAssociatedObject([NSBundle mainBundle], &_bundle, language ? [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:language ofType:@"lproj"]] : nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+@end
