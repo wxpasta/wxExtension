dfc9a384b9f627e508622d19b5c2a1916ac4bf31
许宝吉 <xubaoji@xylink.com>
2019-07-04 15:14:34 +0800

    add Swift

---
+++ Swift/WXExtension/WXExtension/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WXExtension
+//
+//  Created by 许宝吉 on 2019/7/3.
+//  Copyright © 2019 许宝吉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        if self.traitCollection.forceTouchCapability == .available {
+            
+        }
+    }
+
+
+}
+
---
+++ Swift/WXExtension/WXExtensionTests/WXExtensionTests.swift
@@ -0,0 +1,34 @@
+//
+//  WXExtensionTests.swift
+//  WXExtensionTests
+//
+//  Created by 许宝吉 on 2019/7/3.
+//  Copyright © 2019 许宝吉. All rights reserved.
+//
+
+import XCTest
+@testable import WXExtension
+
+class WXExtensionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
